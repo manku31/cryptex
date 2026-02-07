b8a56cc7e34e1919c5be722d2c883ba42049a289
Prashant Singh <prashantdineshsingh48@gmail.com>
2026-02-07 18:49:25 +0530

    Add theme toggle button on navbar

--- components/navbar/ModeToggle.tsx
+++ components/navbar/ModeToggle.tsx
@@ -1,9 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+
+import { Switch } from "../ui/switch";
+
 const ModeToggle = () => {
-    return (
-        <button className="p-2 rounded-md bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200">
-            Toggle Mode
-        </button>
-    );
-}
+  const { theme, setTheme } = useTheme();
+  //   const [mounted, setMounted] = useState(false);
+
+  //   useEffect(() => {
+  //     setMounted(true);
+  //   }, []);
+
+  //   if (!mounted) return null;
+
+  const isDark =
+    theme === "dark" ||
+    (theme === "system" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Sun className="h-4 w-4" />
+      <Switch
+        checked={isDark}
+        onCheckedChange={(checked) => setTheme(checked ? "dark" : "light")}
+      />
+      <Moon className="h-4 w-4" />
+    </div>
+  );
+};
 
 export default ModeToggle;
